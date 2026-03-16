deee91c8cc558543a830dbf1af6e90fa28ddb900
Fauzi Kurniawan <kirigaya.kurniawan@gmail.com>
2026-03-16 07:33:13 +0000

    fix: update .gitignore and improve Dockerfile and build script

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM golang:1.15-alpine
 
 WORKDIR /go/src/github.com/dicodingacademy/karsajobs
-ENV GO111MODULE=on
-ENV APP_PORT=8080
+ENV GO111MODULE=on \
+    APP_PORT=8080
 
 COPY go.mod .
 COPY go.sum .
--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Load .env
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
+
 # SH Variable
 GHCR_USER="kurniawan26"
 IMAGE_NAME="karsajobs"
@@ -8,7 +13,7 @@ IMAGE_NAME="karsajobs"
 docker build -t ghcr.io/$GHCR_USER/$IMAGE_NAME:latest .
 
 # 2. Login ke GHCR
-docker login ghcr.io -u $GHCR_USER
+echo $GHCR_PAT | docker login ghcr.io -u $GHCR_USER --password-stdin
 
 # 3. Push Image
 docker push ghcr.io/$GHCR_USER/$IMAGE_NAME:latest
